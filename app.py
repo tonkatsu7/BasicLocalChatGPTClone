e036cb3cfbbd22054ab00310695566c8b077928e
tonkatsu7 <sipham@gmail.com>
2024-04-24 18:43:36 +0700

    added gradio interface

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import gradio as gr
 
 url = "http://localhost:11434/api/generate"
 
@@ -7,18 +8,28 @@ headers = {
     'Content-Type': 'application/json'
 }
 
-data = {
-    "model": "llama2",
-    "stream": False,
-    "prompt": "Why elephants are afraid of mice?"
-}
+def generate_response(prompt):
+    data = {
+        "model": "llama2",
+        "stream": False,
+        "prompt": prompt
+    }
 
-response = requests.post(url, headers=headers, data=json.dumps(data))
+    response = requests.post(url, headers=headers, data=json.dumps(data))
 
-if response.status_code == 200:
-    response_text = response.text
-    data = json.loads(response_text)
-    actual_response = data["response"]
-    print(actual_response)
-else:
-    print("Error: ", response.status_code, response.text)
+    if response.status_code == 200:
+        response_text = response.text
+        data = json.loads(response_text)
+        actual_response = data["response"]
+        print(actual_response)
+        return actual_response
+    else:
+        print("Error: ", response.status_code, response.text)
+
+iface = gr.Interface(fn=generate_response, 
+                     inputs=["text"], 
+                     outputs=["text"]
+                    )
+
+if __name__ == "__main__":
+    iface.launch()
